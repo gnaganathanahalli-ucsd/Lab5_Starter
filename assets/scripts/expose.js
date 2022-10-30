7ef65c81a691277fe7efd3bb439a9d883570a283
gnamithan <gnaganathanahalli@ucsd.edu>
2022-10-29 21:31:58 -0700

    testing getting option

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,36 +4,28 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
-  var air_opt = document.getElementById("horn-select").value("air-horn");
-  var car_opt = document.getElementById("horn-select").value("car-horn");
-  var party_opt = document.getElementById("horn-select").value("party-horn");
-  var sound_img = document.querySelector('img[alt="No image selected]');
-  // switches image and sound to air horn
-  air_opt.addEventListener("input", function() {
-    if (air_opt.checked) {
-        sound_img.src = "assets\images\air-horn.svg";
-    }
-  });
-
-
+  var horn = document.getElementsByName("horn");
+  var hornSelected = horn.options[horn.selectedIndex].value;
+  if(hornSelected == 'select'){
+    alert("Please select a card type");
+  }
 }
 
 
 
 
 
-// switches image and sound to car horn
-car_opt.addEventListener("input", function() {
-  if (car_opt.checked) {
-      sound_img.src = "assets\images\car-horn.svg";
-  }
-});
-
-// switches image and sound to party horn
-party_opt.addEventListener("input", function() {
-  if (party_opt.checked) {
-      sound_img.src = "assets\images\party-horn.svg";
-  }
-});
 
 
+
+
+// 1. access which option is chosen
+// 2. change image + sound
+// 3. activate button to play sound
+// 4. have confetti go off for particular option
+// 5. access current volume level
+// 6. change icon based on volume level
+// 7. change volume based on volume level
+// 8. test that it all works
+// 9. push to github
+// 10. check on github pages that it all works
