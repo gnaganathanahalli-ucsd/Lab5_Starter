92b140e18659540dbe6010249b3c5f82649d5cd3
gnamithan <gnaganathanahalli@ucsd.edu>
2022-10-29 21:06:21 -0700

    change images

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,41 +4,33 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var air_opt = document.getElementById("air-horn");
+  var car_opt = document.getElementById("car-horn");
+  var party_opt = document.getElementById("party-horn");
+  var sound_img = document.querySelector('img[alt="No image selected]');
 }
 
-var air_opt = document.getElementById("air-horn");
-var car_opt = document.getElementById("car-horn");
-var party_opt = document.getElementById("party-horn");
-var aud = document.getElementsByClassName("hidden");
-
-const buttons = document.querySelectorAll("button");
 
 
 // switches image and sound to air horn
 air_opt.addEventListener("input", function() {
   if (air_opt.checked) {
-      aud.src = "assets\audio\air-horn.mp3";
+      sound_img.src = "assets\images\air-horn.svg";
   }
 });
 
 // switches image and sound to car horn
 car_opt.addEventListener("input", function() {
   if (car_opt.checked) {
-      aud.src = "assets\audio\car-horn.mp3";
+      sound_img.src = "assets\images\car-horn.svg";
   }
 });
 
 // switches image and sound to party horn
 party_opt.addEventListener("input", function() {
   if (party_opt.checked) {
-      aud.src = "assets\audio\party-horn.mp3";
+      sound_img.src = "assets\images\party-horn.svg";
   }
-});
-
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    aud.play();
-  });
 });
 
 
